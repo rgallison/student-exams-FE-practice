08503740a1143c23bbda49ad3f45088ce65cb4a7
Renee Gallison <reneeggallison@gmail.com>
2022-02-05 15:57:45 -0800

    add sidebar and listen for its events

--- src/static/app.component.js
+++ src/static/app.component.js
@@ -1,13 +1,15 @@
 import './components/header-nav/header-nav.component.js';
 import './components/loading-spinner/loading-spinner.component.js';
 import './components/data-table/data-table.component.js';
+import './components/sidebar-nav/sidebar-nav.component.js';
 
 const template = document.createElement('template');
 template.innerHTML = `
     <link rel="stylesheet" href="app.style.css"/>
     <header-nav></header-nav>
+    <sidebar-nav></sidebar-nav>
     <main>
-        <data-table type="exams" src="/api/v1/exams"></data-table>
+        <data-table type="exams" src="/api/v1/exams" columns="id,average,studentCount"></data-table>
     </main>`;
 
 class AppContainer extends HTMLElement {
@@ -18,7 +20,15 @@ class AppContainer extends HTMLElement {
 
     connectedCallback () {
         this.shadowRoot.appendChild(template.content.cloneNode(true));
+
+        this.addEventListener('navigate', ev => {
+            this.shadowRoot.querySelector('data-table').setAttribute('type', ev.detail.navTo);
+            this.shadowRoot.querySelector('data-table').setAttribute('src', ev.detail.data.src);
+            this.shadowRoot.querySelector('data-table').setAttribute('columns', ev.detail.data.columns);
+        });
     }
 }
+
+// sidebarNav.addEventListener('nav-clicked', ev => console.log(ev));
 
 window.customElements.define('app-container', AppContainer);
